01de372d0a91c3219ac7e807bd9a0d105e77ea71
Robby Robinson <robby.robinson@slalom.com>
2022-10-13 11:47:25 -0700

    auth updated to support podcast_id parameter

--- tap_podbean/auth.py
+++ tap_podbean/auth.py
@@ -1,28 +1,45 @@
 """Auth handling for PodbeanStream."""
 
+from typing import Union
 from singer_sdk.authenticators import OAuthAuthenticator
+from singer_sdk.streams import RESTStream
 
 class PodbeanAuthenticator(OAuthAuthenticator):
+    def __init__(
+        self,
+        stream: RESTStream,
+        podcast_id: Union[str, None] = None,
+        default_expiration: Union[int, None] = None,
+    ) -> None:
+        """Create a new authenticator.
+        Args:
+            stream: The stream instance to use with this authenticator.
+            podcast_id: API username.
+            default_expiration: Default token expiry in seconds.
+        """
+        expiration = default_expiration or stream.config.get('auth_expires_in') or None
+        super().__init__(stream=stream, default_expiration=expiration)
+        self.url_base = stream.url_base
+        self.podcast_id = podcast_id
 
     @property
     def auth_endpoint(self) -> str:
+        auth_endpoint = f'{self.url_base}/v1/oauth/token'
         basic_auth = f'{self.client_id}:{self.client_secret}@'
-        return f'{self._auth_endpoint}'.replace('://', f'://{basic_auth}')
+        return f'{auth_endpoint}'.replace('://', f'://{basic_auth}')
 
     @property
     def oauth_request_body(self) -> dict:
         payload = {'grant_type': 'client_credentials'}
 
-        # Optional parameter, uses default podcast otherwise
-        # podcast_id:str = self.podcast_id
-        # if podcast_id:
-        #     payload['podcast_id'] = podcast_id
+        if self.podcast_id:
+             payload['podcast_id'] = self.podcast_id
 
         return payload
 
     @property
     def auth_headers(self) -> dict:
-        """do not remove; overrides default auth_headers"""
+        """do not remove; overrides default population of auth_headers"""
         return {}
 
     @property
