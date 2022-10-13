837d1b26102c42f63803f7ddf8f8f94ce0359ebd
Robby Robinson <robby.robinson@slalom.com>
2022-10-13 15:16:22 -0700

    moved paginator to client

--- tap_podbean/client.py
+++ tap_podbean/client.py
@@ -2,10 +2,10 @@
 
 from pathlib import Path
 from typing import Any, Dict, Optional, Union, List, Iterable
-from isort import stream
 from memoization import cached
 from singer_sdk.streams import RESTStream
 from tap_podbean.auth import PodbeanAuthenticator
+from tap_podbean.paginator import PodbeanPaginator
 
 
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
@@ -21,36 +21,33 @@ class PodbeanStream(RESTStream):
     @property
     def authenticator(self) -> PodbeanAuthenticator:
         """Return a new authenticator object."""
-        endpoint = f'{self.url_base}/v1/oauth/token'
-        expiration:int = self.config.get('auth_expires_in')
+        return PodbeanAuthenticator(self)
 
-        return PodbeanAuthenticator(self,
-            auth_endpoint=endpoint,
-            default_expiration=expiration
-        )
+    def get_new_paginator(self) -> PodbeanPaginator:
+        """Get a fresh paginator for this API endpoint."""
+        return PodbeanPaginator()
 
-    @property
-    def page_limit(self) -> Union[int, None]:
-        """Return the page limit for paginated streams."""
-        return None
-
+    #@property
+    #def additional_url_params(self) -> dict:
+    #    return {}
 
     def get_url_params(
-        self, context: Optional[dict], next_page_token: Optional[Any]
+        self, context: Optional[dict], next_page_token: Optional[int]
     ) -> Dict[str, Any]:
         """Return a dictionary of values to be used in URL parameterization."""
         params = {}
 
-        if self.page_limit:
-            params['limit'] = self.page_limit
-
         if next_page_token:
             params["offset"] = next_page_token
 
+            page_limit:int = self.config.get('page_limit')
+            if page_limit:
+                params['limit'] = page_limit
+        
         if context:
-            stream_params:dict = context
-            if stream_params:
-                for k, v in stream_params.items():
-                    params[k] = v
-
+            partitions = {k:v for k,v in context.items() if k in ['podcast_id', 'year']}
+            
+            for k,v in partitions.items():
+                params[k] = v
+            
         return params
