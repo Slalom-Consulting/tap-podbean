0f89d1424c7a34661ae8557f3964bf3e93c3bee6
Robby Robinson <robby.robinson@slalom.com>
2022-10-18 20:30:26 -0700

    cleaned up client

--- tap_podbean/client.py
+++ tap_podbean/client.py
@@ -1,15 +1,14 @@
 """REST client handling, including PodbeanStream base class."""
 
 from pathlib import Path
-from typing import Any, Dict, Optional, Union, List, Iterable
-from memoization import cached
+from typing import Any, Dict, Optional
+#from memoization import cached
 from singer_sdk.streams import RESTStream
 from tap_podbean.auth import PodbeanAuthenticator
 from tap_podbean.paginator import PodbeanPaginator
 
 
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
-
 class PodbeanStream(RESTStream):
     """Podbean stream class."""
 
@@ -27,27 +26,17 @@ class PodbeanStream(RESTStream):
         """Get a fresh paginator for this API endpoint."""
         return PodbeanPaginator()
 
-    #@property
-    #def additional_url_params(self) -> dict:
-    #    return {}
-
     def get_url_params(
         self, context: Optional[dict], next_page_token: Optional[int]
     ) -> Dict[str, Any]:
         """Return a dictionary of values to be used in URL parameterization."""
         params = {}
-
+        
         if next_page_token:
             params["offset"] = next_page_token
 
             page_limit:int = self.config.get('page_limit')
             if page_limit:
-                params['limit'] = page_limit
-        
-        if context:
-            partitions = {k:v for k,v in context.items() if k in ['podcast_id', 'year', 'types']}
-            
-            for k,v in partitions.items():
-                params[k] = v
+                params['limit'] = page_limit  
             
         return params
