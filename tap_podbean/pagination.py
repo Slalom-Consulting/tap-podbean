62a91d1b48067501df0327869b4fcd93a2bb481b
Robby Robinson <robby.robinson@slalom.com>
2022-11-04 11:27:39 -0700

    clarity

--- tap_podbean/pagination.py
+++ tap_podbean/pagination.py
@@ -4,28 +4,21 @@ from typing import Any
 from singer_sdk.pagination import BaseOffsetPaginator
 from requests import Response
 
-PAGINATION_DEFAULT = 20
-PAGINATION_MAX = 100
+PAGE_SIZE = 20
+PAGE_SIZE_MAX = 100
 
 
 class PodbeanPaginator(BaseOffsetPaginator):
     def __init__(
         self,
         start_value: int,
-        page_size: int = PAGINATION_DEFAULT,
+        page_size: int,
         *args: Any,
         **kwargs: Any
     ) -> None:
-        if page_size > PAGINATION_MAX:
-            page_size = PAGINATION_MAX
-
+        page_size = min(page_size or PAGE_SIZE, PAGE_SIZE_MAX)
         super().__init__(start_value, page_size, *args, **kwargs)
 
     def has_more(self, response: Response) -> bool:
-        """Check if the endpoint has any pages left.
-        Args:
-            response: API response object.
-        Returns:
-            Boolean flag used to indicate if the endpoint has more pages.
-        """
-        return response.json().get('has_more', False) == True
+        has_more:bool = response.json().get('has_more', False)
+        return has_more == True
